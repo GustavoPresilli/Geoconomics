18b3d5b92a38927a3a032a88b87985ef4eb661da
GustavoPresilli <gustavo.spresilli@sptech.school>
2024-06-03 20:09:05 -0300

    Cadastro e Login em funcionamento

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -4,35 +4,22 @@ function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
 
-    if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está indefinida!");
+    if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu e-mail undefined!");
     } else {
-
         usuarioModel.autenticar(email, senha)
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        res.status(200).json(resultadoAutenticar)
+
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -47,7 +34,6 @@ function autenticar(req, res) {
                 }
             );
     }
-
 }
 
 function cadastrar(req, res) {
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -2,14 +2,14 @@ var database = require("../database/config")
 
 function autenticar(email, senha) {
     var instrucaoSql = `
-        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    SELECT  nome, TIMESTAMPDIFF(YEAR, dtNasc, CURDATE()) AS idade, genero, email, regiao, senha  FROM Usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function cadastrar(usuario, dataNasc, email, senha, regiao, genero) {
-    
+
     var instrucaoSql = `
         INSERT INTO Usuario (nome, dtNasc, genero, email, regiao, senha) VALUES ('${usuario}', '${dataNasc}', '${genero}', '${email}', '${regiao}', '${senha}');
     `;
