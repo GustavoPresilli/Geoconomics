7a827a898d88a4e97a8d1e22c4a5b0206fcd4b20
GustavoPresilli <gustavo.spresilli@sptech.school>
2024-06-03 18:48:22 -0300

    Banco de dados Geoconomics

---
+++ database/script_geoconomics.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS Geoconomics;
+CREATE DATABASE Geoconomics;
+
+USE Geoconomics;
+
+CREATE TABLE Usuario (
+    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(60) NOT NULL,
+    dtNasc DATE NOT NULL,
+    genero VARCHAR(60) NOT NULL,
+    email VARCHAR(60) UNIQUE NOT NULL,
+    regiao VARCHAR(80) NOT NULL,
+    senha VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE Quiz (
+    idQuiz INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(60) NOT NULL,
+    descricao VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Metricas (
+    idMetricas INT AUTO_INCREMENT PRIMARY KEY,
+    qtdAcertos INT NOT NULL,
+    qtdErros INT NOT NULL,
+    dtTentativa DATE NOT NULL,
+    fkQuiz INT,
+    fkUsuario INT,
+    FOREIGN KEY (fkQuiz) REFERENCES Quiz(idQuiz),
+    FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUsuario)
+);
